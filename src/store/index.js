b682f879d51f4585819d513e9bd9ee838b9d86de
Nyagolova <zlatina.nyagolova@gmail.com>
2020-01-23 18:24:44 +0200

    Add data to store - bookies.json and countries.json

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import bookies from '@/store/bookies.json'
+import countries from '@/store/countries.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    bookmakersData: bookies,
+    countriesData: countries
   },
   mutations: {
   },
