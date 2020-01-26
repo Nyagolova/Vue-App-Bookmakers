f3dc2432aad8463c79091ee8a06a39f42e0feb29
Nyagolova <zlatina.nyagolova@gmail.com>
2020-01-26 15:36:05 +0200

    Add filtration to BookmakersDropdown.vue

--- src/components/BookmakersDropdown.vue
+++ src/components/BookmakersDropdown.vue
@@ -1,46 +1,48 @@
 <template>
     <v-col  class="flex-grow-1 flex-shrink-0" >
         <v-card class="pa-2" elevation=0 tile color="#f1f3f9">
+
             <v-select
-            background-color="white"
+                background-color="white"
+                :items="bookmakersAll"
+                item-text ="name"
+                item-value = "name"
                 dense
                 hide-details 
-                :items="filteredBookmakers"
-                :value="selectedItemTitle"
                 outlined
                 :menu-props="{ lazy: true, offsetOverflow: true, offsetY: true, overflow: true }"
-                v-model="widget.thingId"
-                @click:clear="widget.thingId === null"
             >
                 <template v-slot:prepend-item>
                     <div class="px-2 py-0">
                     <v-text-field
-                        
                         color="grey lighten-2"
                         hide-details
                         outlined
                         dense
                         single-line
-                        v-model="filter"
+                        v-model="inputFilterText"
                     ><v-icon slot="append" >mdi-magnify</v-icon></v-text-field>
                     </div>
                     <v-divider class="my-2"></v-divider>
+                    <div class="px-2 py-0">
+                        <v-list-item-content>Show All</v-list-item-content>
+                    </div>
+                    <v-divider class="my-2"></v-divider>
+                    <div class="px-2 py-0">
+                        Show Active
+                    </div>
+                    <v-divider class="my-2"></v-divider>
+                    <div class="px-2 py-0">
+                        Show Inactive
+                    </div>
+                    <v-divider class="my-2"></v-divider>
                 </template>
-                <template v-slot:item="{ item }">
+                <template v-slot:bookie="{ bookie }">
                     <v-list-item-content>
-                        {{ item.title }}
+                        {{ bookie.name }}
                     </v-list-item-content>
                 </template>
-                <template v-slot:no-data>
-                    <v-layout px-3 py-2>
-                    <v-flex
-                        class="grey--text text--darken-1"
-                        xs12
-                    >
-                        Couldn't find any {{ listPlural.toLowerCase() }} that match <span class="font-weight-medium text--darken-3">{{ filter }}</span> ...
-                    </v-flex>
-                    </v-layout>
-                </template>
+            
             </v-select>
         </v-card>
     </v-col>
@@ -49,70 +51,40 @@
 <script>
 
 export default {
-    // data () {
-    //     return {
-    //         items: ['- SHOW ALL -', '- SHOW ACTIVE -', '- SHOW INACTIVE -', 'Other'],
-    //     }
-    // } 
-
-    data: () => (
-        {
-            selectedItem: '',
-            filter: null,
-            listPlural: 'Things',
-            listSingular: 'Thing',
-            thing: {
-            categoryId: null,
-            description: null,
-            rating: null,
-            title: null,
-            valueTypeId: null
-        },
-        things: [
-            {
-                title: 'Thing One'
-            },
-            {
-                title: 'Thing Two'
-            },
-            {
-                title: 'Thing Three'
-            },
-            {
-                title: 'Thing Four'
-            },
-            {
-                title: 'Thing Five'
-            },
-            {
-                title: 'Thing Six'
-            }
-        ],
-        widget: {
-            thingId: null
+    data () {
+        return {
+            bookmakersData : [],
+            inputFilterText: null,
+            showAll: null,
+            showActive: null,
+            showInactive: null
         }
-  }),
-  computed: {
-    filteredBookmakers () {
-      if (this.filter) {
-        const pattern = new RegExp(`\b?${this.filter}\b?`, 'ig')
-        return this.things.filter(thing => thing.title.toLowerCase().match(pattern) )
-      } else {
-        return this.things
-      }
+    
     },
-    selectedItemTitle () {
-        if (this.widget.thingId) {
-            return this.widget.thingId.title
-        }  else {
-            return ''
+    computed: {
+        bookmakersAll () {
+            //var bookmakersData = this.$store.state.bookmakersData
+
+            if (this.inputFilterText) {
+                const pattern = new RegExp(`\b?${this.inputFilterText}\b?`, 'ig')
+                return this.bookmakersData.filter(thing => thing.name.toLowerCase().match(pattern) )
+            } else {
+                return this.bookmakersData
+            } 
+        },
+        bookmakersActive () {
+            return this.bookmakersData.filter(thing => thing.active === 1)
+        },
+        bookmakersInactive () {
+            return this.bookmakersData.filter(thing => thing.active === 0)
         }
+    },
+    mounted () {
+        this.bookmakersData = this.$store.state.bookmakersData
+    },
+    methods: {
+    
     }
-  },
- 
-  methods: {
- 
-  }
 }
 </script>
 
